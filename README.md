5220a621625046379f4633c41b1f3e06d9a7ec8b
Shaun Burdick <github@shaunburdick.com>
2016-05-22 11:03:24 -0400

    Initial commit of Jira -> Slack functionality

--- README.md
+++ README.md
@@ -1,2 +1,54 @@
-# jira2slack
+# JIRA 2 Slack
+[![Build Status](https://travis-ci.org/shaunburdick/jira2slack.svg?branch=master)](https://travis-ci.org/shaunburdick/gamebot) [![Coverage Status](https://coveralls.io/repos/github/shaunburdick/jira2slack/badge.svg?branch=master)](https://coveralls.io/github/shaunburdick/gamebot?branch=master)  [![js-semistandard-style](https://img.shields.io/badge/code%20style-semistandard-brightgreen.svg)](https://github.com/Flet/semistandard)
+
 Library to convert between JIRA markup and Slack markup
+
+## Example
+```
+const J2S = require('jira2slack');
+
+const slackMD = J2S.toSlack('*This* _is_ -some- ^over^ ~formatted~ {{text}}');
+// *This* _is_ ~some~ ^over -formatted- `text`
+```
+
+## Conversions
+Below are the conversions that are made:
+
+### Jira -> Slack
+| Format | JIRA | Slack | Destructive | Notes |
+| ------ | ---- | ----- | ----------- | ----- |
+| Headers | `h1. Heading` | `\n *Heading*\n` | Yes | Slack doesn't support headers so library converts headers to bolded text on it's own line |
+| Bold   | `*bold*` | `*bold*` | Sometimes | [[1]](notes) |
+| Italic | `_italic_` | `_italic_` | Sometimes | [[1]](notes) |
+| Unordered List | <br>`* Bulleted List`<br>`** Indented more`<br>`* Indented less` | <br>`• Bulleted List`<br>`  • Indented more`<br>`• Indented less` | No | |
+| Unordered Dash List | `- Bulleted Dash List`<br>`- Bulleted Dash List`<br>`- Bulleted Dash List` | `• Bulleted Dash List`<br>`• Bulleted Dash List`<br>`• Bulleted Dash List` | Yes | Ambigious result from Bulleted list so reversal results in use of astericks |
+|Ordered List | `# Numbered List`<br>`## Indented more`<br>`## Indented more`<br>`### Indented morer`<br>`### Indented morer`<br>`### Indented morer`<br>`## Indented more`<br>`# Indented less` | `1. Numbered List`<br>`  1. Indented more`<br>`  2. Indented more`<br>`    1. Indented morer`<br>`    2. Indented morer`<br>`    3. Indented morer`<br>`  3. Indented more`<br>`2. Indented less` | No | |
+| Monospaced | `{{code}}` | `` `code` `` | No | |
+| Citations | `??citation??` | `-- citation_` | Yes | Slack doesn't have citations, but I can replicate resulting format |
+| Subscript | `~subscript~` | `_subscript` | Yes | Slack doesn't have subscript, but I denote it with a single underscore |
+| Superscript | `^superscript^` | `^superscript` | Yes | Slack doesn't have superscript, but I denote it with a single carret |
+| Strikethrough | `-strikethrough-` | `~strikethrough~` | Sometimes | [[1]](notes) |
+| Code | `{code}some code{code}` | `` ```some code``` `` | Yes | Slack doesn't support specify the language, so that is removed during conversion |
+| Quotes | `{quote}quoted text{quote}` | `` ```quoted text``` `` | Yes | Slack does't support quoted text so the library uses preformmated `` ``` `` instead. This makes it impossible to convert back. |
+| Pre-formatted Text | `{noformat}pre text{noformat}` | `` ```pre text``` `` | Yes | Slack does't support no format text so the library uses preformmated `` ``` `` instead. This makes it impossible to convert back. |
+| Unnamed Link | `[http://someurl.com]` | `<http://someurl.com>` | No | |
+| Named Link | `Someurl|http://someurl.com]` | `<http://someurl.com|Someurl>` | No | |
+| Blockquote | `bq. This is quoted` | `> This is quoted` | Sometimes | Slack doesn't support multi-line blockquotes (>>>) so thos are ignored |
+| Color | `{color:white}This is white text{color}` | `This is white text` | Yes | Slack doesn't support colored text so it is removed. |
+| Panel | `{panel:title=foo}Panel Contents{panel}` | Panel: <br>&#124; foo &#124;<br>&#124; --- &#124;<br>&#124; Panel Contents &#124; | Yes | Slack doesn't support panels, so the library attempts to replicate the format. This makes it impossible to convert back. |
+| Table | &#124;&#124;heading 1&#124;&#124;heading 2&#124;&#124;<br>&#124;col A1&#124;col B1&#124;<br>&#124;col A2&#124;col B2&#124; | &#124;heading 1&#124;heading 2&#124;<br>&#124; --- &#124; --- &#124;<br>&#124;col A1&#124;col B1&#124;<br>&#124;col A2&#124;col B2&#124; | No | |
+
+
+### Notes
+1. JIRA supports spaces at the beginning and end of a markdown section (ex `_ foo _`) where Slack does not. To match the formatting, the library moves those spaces outside the formatter (ex `_ foo _` becomes ` _foo_ `). While the result works in both JIRA and Slack, it is technically destructive.
+
+## Acknowledgments
+Special thanks to Kyle Farris for his [JIRA to Markdown library](https://github.com/kylefarris/J2M)
+
+## Contributing
+1. Create a new branch, please don't work in master directly.
+2. Add failing tests for the change you want to make (if appliciable). Run `npm test` to see the tests fail.
+3. Fix stuff.
+4. Run `npm test` to see if the tests pass. Repeat steps 2-4 until done.
+5. Update the documentation to reflect any changes.
+6. Push to your fork and submit a pull request.
---
+++ test.js
@@ -0,0 +1,239 @@
+const test = require('tape');
+const J2S = require('./index.js');
+
+test('JIRA to Slack: Check Individual Formatting', (assert) => {
+  assert.equal(
+    J2S.toSlack('h1. Heading'),
+    '\n *Heading*\n',
+    'Headings'
+  );
+
+  assert.equal(
+    J2S.toSlack('* Bulleted List\n** Indented more\n* Indented less\n\n'),
+    '• Bulleted List\n  • Indented more\n• Indented less\n\n',
+    'Unordered List'
+  );
+
+  assert.equal(
+    J2S.toSlack('- Bulleted Dash List\n- Bulleted Dash List\n- Bulleted Dash List\n\n'),
+    '• Bulleted Dash List\n• Bulleted Dash List\n• Bulleted Dash List\n\n',
+    'Unordered Dash List'
+  );
+
+  assert.equal(
+    J2S.toSlack(
+      '# Numbered List\n' +
+      '## Indented more\n' +
+      '## Indented more\n' +
+      '### Indented morer\n' +
+      '### Indented morer\n' +
+      '### Indented morer\n' +
+      '## Indented more\n' +
+      '# Indented less\n\n'
+    ),
+    '1. Numbered List\n' +
+    '  1. Indented more\n' +
+    '  2. Indented more\n' +
+    '    1. Indented morer\n' +
+    '    2. Indented morer\n' +
+    '    3. Indented morer\n' +
+    '  3. Indented more\n' +
+    '2. Indented less\n\n',
+    'Ordered List'
+  );
+
+  assert.equal(
+    J2S.toSlack('||heading 1||heading 2||\n|col A1|col B1|\n|col A2|col B2|\n\n'),
+    '\n|heading 1|heading 2|\n| --- | --- |\n|col A1|col B1|\n|col A2|col B2|\n\n',
+    'Table'
+  );
+
+  assert.equal(
+    J2S.toSlack('Bold: *boldy*\n'),
+    'Bold: *boldy*\n',
+    'Bold'
+  );
+
+  assert.equal(
+    J2S.toSlack('Bold (spaced): * boldy is spaced *\n'),
+    'Bold (spaced):  *boldy is spaced* \n',
+    'Bold (spaced)'
+  );
+
+  assert.equal(
+    J2S.toSlack('Italic: _italicy_\n'),
+    'Italic: _italicy_\n',
+    'Italic'
+  );
+
+  assert.equal(
+    J2S.toSlack('Italic (spaced): _italicy is poorly spaced _\n'),
+    'Italic (spaced): _italicy is poorly spaced_ \n',
+    'Italic (spaced)'
+  );
+
+  assert.equal(
+    J2S.toSlack('Monospace: {{$code}}\n'),
+    'Monospace: `$code`\n',
+    'Monospace'
+  );
+
+  assert.equal(
+    J2S.toSlack('Citations: ??citation??\n'),
+    'Citations: _-- citation_\n',
+    'Citations'
+  );
+
+  assert.equal(
+    J2S.toSlack('Subscript: ~subscript~\n'),
+    'Subscript: _subscript\n',
+    'Subscript'
+  );
+
+  assert.equal(
+    J2S.toSlack('Superscript: ^superscript^\n'),
+    'Superscript: ^superscript\n',
+    'Superscript'
+  );
+
+  assert.equal(
+    J2S.toSlack('Strikethrough: -strikethrough-\n'),
+    'Strikethrough: ~strikethrough~\n',
+    'Strikethrough'
+  );
+
+  assert.equal(
+    J2S.toSlack('Not Strikethrough: i-use-dashes\n'),
+    'Not Strikethrough: i-use-dashes\n',
+    'No strikethrough for dashed words'
+  );
+
+  assert.equal(
+    J2S.toSlack('Strikethrough (spaced): - strikethrough is poorly spaced-\n'),
+    'Strikethrough (spaced):  ~strikethrough is poorly spaced~\n',
+    'Strikethrough (spaced)'
+  );
+
+  assert.equal(
+    J2S.toSlack('Code: {code}some code{code}\n'),
+    'Code: ```some code```\n',
+    'Code'
+  );
+
+  assert.equal(
+    J2S.toSlack('Quote: {quote}quoted text{quote}\n'),
+    'Quote: ```quoted text```\n',
+    'Quote'
+  );
+
+  assert.equal(
+    J2S.toSlack('No Format: {noformat}pre text{noformat}\n'),
+    'No Format: ```pre text```\n',
+    'Pre-formatted Text'
+  );
+
+  assert.equal(
+    J2S.toSlack('Unnamed Link: [http://someurl.com]\n'),
+    'Unnamed Link: <http://someurl.com>\n',
+    'Unnamed Link'
+  );
+
+  assert.equal(
+    J2S.toSlack('Named Link: [Someurl|http://someurl.com]\n'),
+    'Named Link: <http://someurl.com|Someurl>\n',
+    'Named Link'
+  );
+
+  assert.equal(
+    J2S.toSlack('Blockquote: \nbq. This is quoted\n'),
+    'Blockquote: \n> This is quoted\n',
+    'Blockquote'
+  );
+
+  assert.equal(
+    J2S.toSlack('Color: {color:white}This is white text{color}\n'),
+    'Color: This is white text\n',
+    'Color'
+  );
+
+  assert.equal(
+    J2S.toSlack('Panel: {panel:title=foo}Panel Contents{panel}\n'),
+    'Panel: \n| foo |\n| --- |\n| Panel Contents |\n',
+    'Panel'
+  );
+
+  assert.end();
+});
+
+test('JIRA to Slack: Check All Formatting', (assert) => {
+  const jiraFormat = 'h1. Heading\nFoo foo _foo_ foo foo foo\n' +
+    '* Bulleted List\n** Indented more\n* Indented less\n\n' +
+    '- Bulleted Dash List\n- Bulleted Dash List\n- Bulleted Dash List\n\n' +
+    '# Numbered List\n' +
+    '## Indented more\n' +
+    '## Indented more\n' +
+    '### Indented morer\n' +
+    '### Indented morer\n' +
+    '### Indented morer\n' +
+    '## Indented more\n' +
+    '# Indented less\n\n' +
+    '||heading 1||heading 2||\n' +
+    '|col A1|col B1|\n|col A2|col B2|\n\n' +
+    'Bold: *boldy*\n' +
+    'Bold (spaced): * boldy is spaced *\n' +
+    'Italic: _italicy_\n' +
+    'Italic (spaced): _italicy is poorly spaced _\n' +
+    'Monospace: {{$code}}\n' +
+    'Citations: ??citation??\n' +
+    'Subscript: ~subscript~\n' +
+    'Superscript: ^superscript^\n' +
+    'Strikethrough: -strikethrough-\n' +
+    'Not Strikethrough: i-use-dashes\n' +
+    'Strikethrough (spaced): - strikethrough is poorly spaced-\n' +
+    'Code: {code}some code{code}\n' +
+    'Quote: {quote}quoted text{quote}\n' +
+    'No Format: {noformat}pre text{noformat}\n' +
+    'Unnamed Link: [http://someurl.com]\n' +
+    'Named Link: [Someurl|http://someurl.com]\n' +
+    'Blockquote: \nbq. This is quoted\n' +
+    'Color: {color:white}This is white text{color}\n' +
+    'Panel: {panel:title=foo}Panel Contents{panel}\n';
+
+  const expectedText = '\n *Heading*\n\nFoo foo _foo_ foo foo foo\n' +
+    '• Bulleted List\n  • Indented more\n• Indented less\n\n' +
+    '• Bulleted Dash List\n• Bulleted Dash List\n• Bulleted Dash List\n\n' +
+    '1. Numbered List\n' +
+    '  1. Indented more\n' +
+    '  2. Indented more\n' +
+    '    1. Indented morer\n' +
+    '    2. Indented morer\n' +
+    '    3. Indented morer\n' +
+    '  3. Indented more\n' +
+    '2. Indented less\n\n' +
+    '\n|heading 1|heading 2|\n' +
+    '| --- | --- |\n|col A1|col B1|\n|col A2|col B2|\n\n' +
+    'Bold: *boldy*\n' +
+    'Bold (spaced):  *boldy is spaced* \n' +
+    'Italic: _italicy_\n' +
+    'Italic (spaced): _italicy is poorly spaced_ \n' +
+    'Monospace: `$code`\n' +
+    'Citations: _-- citation_\n' +
+    'Subscript: _subscript\n' +
+    'Superscript: ^superscript\n' +
+    'Strikethrough: ~strikethrough~\n' +
+    'Not Strikethrough: i-use-dashes\n' +
+    'Strikethrough (spaced):  ~strikethrough is poorly spaced~\n' +
+    'Code: ```some code```\n' +
+    'Quote: ```quoted text```\n' +
+    'No Format: ```pre text```\n' +
+    'Unnamed Link: <http://someurl.com>\n' +
+    'Named Link: <http://someurl.com|Someurl>\n' +
+    'Blockquote: \n> This is quoted\n' +
+    'Color: This is white text\n' +
+    'Panel: \n| foo |\n| --- |\n| Panel Contents |\n';
+
+  const response = J2S.toSlack(jiraFormat);
+
+  assert.equal(response, expectedText, 'JIRA Markup should be converted to Slack Markup');
+  assert.end();
+});
